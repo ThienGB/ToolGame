a1b4f484a07731bcbdff18a8846db8399ddcb1e6
M94 <vietphat091103@gmail.com>
2026-05-19 15:09:39 +0700

    Phat sua

--- mega-uplv-lq(LD)/gui_tool_boxphone.py
+++ mega-uplv-lq(LD)/gui_tool_boxphone.py
@@ -1079,7 +1079,7 @@ class AutoClickerInstance:
         
         # 4. GIAI ĐOẠN GHÉP ĐỘI (TEAM UP)        
         teamup_host_script = [
-            {"action": "press_esc", "wait": 2},
+            
             {"action": "click_image", "target": "images_boxphone/dau_thuong1.png", "timeout": 60, "confidence": 0.7},
             
             {"action": "click_image_if", "target": "images_boxphone/x1.png", "timeout": 5, "confidence": 0.7},
