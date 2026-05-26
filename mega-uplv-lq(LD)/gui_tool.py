2d4b676e35ab3fda2f98d20e4550ec31983528c6
M94 <vietphat091103@gmail.com>
2026-05-26 18:54:22 +0700

    Phat sua

--- mega-uplv-lq(LD)/gui_tool.py
+++ mega-uplv-lq(LD)/gui_tool.py
@@ -1024,7 +1024,7 @@ class AutoClickerInstance:
         
         # 1. GIAI ĐOẠN LOGIN
         login_script = [
-            {"action": "click_image_if", "target": "images/game_logo.png", "timeout": 10, "confidence": 0.7},
+            {"action": "open_game"},
             {"action": "click_image_if", "target": "images/xacnhan.png", "timeout": 30, "confidence": 0.7},
             {"action": "click_image_if", "target": "images/xacnhan.png", "timeout": 5, "confidence": 0.7},
             {"action": "click_image", "target": "images/login_garena.png", "timeout": 420, "confidence": 0.9},
